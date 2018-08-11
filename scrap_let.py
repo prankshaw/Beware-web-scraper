66a2f56f825319c4bb8248287cb7b026076ba317
Pranjal <pranjalrastogi1998@gmail.com>
2018-08-11 22:55:44 +0530

    added where path change is required

--- scrap_let.py
+++ scrap_let.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.common.exceptions import TimeoutException 
 driver_option = webdriver.ChromeOptions()
-chromedriver_path =r'C:/Users/pranjal/Desktop/softwares/chromedriver/chromedriver_win32/chromedriver.exe' 
+chromedriver_path =r'C:/Users/pranjal/Desktop/softwares/chromedriver/chromedriver_win32/chromedriver.exe' # change the path here
 def create_webdriver():
     return webdriver.Chrome(executable_path=chromedriver_path) 
 
